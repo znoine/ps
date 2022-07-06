77b40b641378cb758cf025318902c83a8aff1320
znoine <znoine@znoineui-MacBookPro.local>
2022-07-06 19:40:31 +0900

    solved: Fruit Into Baskets

---
+++ leetcode/fruit-into-baskets.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+ const totalFruit = (fruits) => {
+  const sp = [], pick = [];
+  let len = 0, max = 0;
+  for (let i = 0; i < fruits.length; i++) {
+      const fruit = fruits[i];
+      if (i === 0 || fruits[i - 1] !== fruit) sp[fruit] = i;
+      if (!pick.includes(fruit)) pick.push(fruit);
+      len++;
+      if (len > max) max = len;
+      if (pick.length === 3) {
+          const [fruitOne, fruitTwo] = pick;
+          pick.splice(sp[fruitOne] > sp[fruitTwo] ? 1 : 0, 1);
+          len = 1;
+      }
+  }
+  return max;
+};
